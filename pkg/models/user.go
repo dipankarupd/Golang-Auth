5b6b083d4f6e8486ece35eabe930b97a4c7f8e09
Dipankar <drupd17@gmail.com>
2024-05-26 18:27:50 +0530

    model create

---
+++ pkg/models/user.go
@@ -0,0 +1,18 @@
+package models
+
+import (
+	"time"
+
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+type User struct {
+	Id           primitive.ObjectID `bson:"_id"`
+	Name         *string            `json:"name,omitempty" validate:"required,min=2,max=100"`
+	Email        *string            `json:"email" validate:"email,required"`
+	Password     *string            `json:"password" validate:"required,min=6"`
+	AccessToken  *string            `json:"access_token"`
+	RefreshToken *string            `json:"request_token"`
+	CreatedAt    time.Time          `json:"created_at"`
+	UpdatedAt    time.Time          `json:"updated_at"`
+}
